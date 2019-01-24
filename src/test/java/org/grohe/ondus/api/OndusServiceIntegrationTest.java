88af85b849365f6836de554bc1e8b02ab4a168bd
Florian <florian.schmidt.welzow@t-online.de>
2019-01-24 22:32:44 +0100

    Add integration test to really interact with the GROHE API, at least for the login

---
+++ src/test/java/org/grohe/ondus/api/OndusServiceIntegrationTest.java
@@ -0,0 +1,22 @@
+package org.grohe.ondus.api;
+
+import org.junit.Test;
+
+import javax.security.auth.login.LoginException;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+public class OndusServiceIntegrationTest {
+    @Test
+    public void wrongUsername_returns441() throws Exception {
+        try {
+            OndusService.login("a-test", "a-password");
+        } catch (LoginException e) {
+            assertThat(e.getMessage(), equalTo("441 - Unauthorized"));
+            return;
+        }
+        fail("Should throw LoginException");
+    }
+}
