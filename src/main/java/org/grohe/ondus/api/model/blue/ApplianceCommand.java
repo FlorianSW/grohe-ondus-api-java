ab3cb06891e5c5824799dca09e2ff08ee9856cd7
Florian <florian.schmidt.welzow@t-online.de>
2020-03-05 19:14:02 +0100

    Fix wrong import

--- src/main/java/org/grohe/ondus/api/model/blue/ApplianceCommand.java
+++ src/main/java/org/grohe/ondus/api/model/blue/ApplianceCommand.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.grohe.ondus.api.model.BaseApplianceCommand;
-import org.grohe.ondus.api.model.guard.Appliance;
 
 @Getter
 @Setter
